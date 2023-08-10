bc1814f50c5816113fe3e648008a15bf94b5f63b
Alex_Nout <calium_by@mail.ru>
2023-08-10 13:15:05 +0300

    done

--- src/index.js
+++ src/index.js
@@ -37,10 +37,68 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
-    // write your solution here
+ //split coded string into separated letters  
+ let arr = expr.split('');
+ let sepArr = [];
+ let i = 0;
+ while (i <arr.length) {
+     sepArr.push(arr.slice(i, i + 10));
+     i += 10
+ }
+ 
+//delete padding 0 for each letter
+ let noPaddingSepArr = [];
+ let letterArr = [];
+ let isStarted = false;
+ sepArr.forEach((letter) => {
+     for (let i = 0; i < letter.length; i++ ) {
+         if (letter[i] == 1 || letter[i] == '*') {
+             isStarted = true;
+         }
+         if(isStarted) {
+             letterArr.push(letter[i])
+         }
+     }
+     noPaddingSepArr.push(letterArr);
+     letterArr = [];
+     isStarted = false;
+ })
+
+//convert 1 and 0 to . and -
+ let morseArr = [];
+ let morseSyl = [];
+ noPaddingSepArr.forEach((letter)=>{
+     for(let i = 1; i< letter.length; i += 2) {
+         if (letter[i] == '*') {
+             morseSyl.push(' ');
+             break;
+         } else if (letter[i] == 0) {
+         morseSyl.push('.')
+         } else if (letter[i] == 1) {
+             morseSyl.push('-')
+         }
+     }
+     morseArr.push(morseSyl.join(''));
+     morseSyl = [];
+ })
+
+// now decode!
+let decodedArr = [];
+for(let i = 0; i < morseArr.length; i++) {
+ if (morseArr[i] == ' ') {
+     decodedArr.push(' ')
+ } else {
+     decodedArr.push(MORSE_TABLE[morseArr[i]])
+ }
+}
+const decodedStirng = decodedArr.join('');
+
+    return decodedStirng
 }
 
 module.exports = {
     decode
 }
+
